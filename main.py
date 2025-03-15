35348c5c5403ef377855e0068c3b88128347ccc4
Gustavo Henrique <108089562+gustavohpereira@users.noreply.github.com>
2025-03-15 09:31:46 -0300

    :sparkles: feat: Criação de API basica

--- main.py
+++ main.py
@@ -10,13 +10,37 @@
 # print("Resposta do modelo 2:", resposta2)
 
 
+
 from llm_chatbot import Model
+from fastapi import FastAPI
+from pydantic import BaseModel
 
-chatbot_qwen = Model()
-chatbot_deepseek = Model('deepseek-r1-distill-llama-70b')
+app = FastAPI()
 
-resposta1 = chatbot_qwen.chat("O que é Alzheimer?")
-print("Resposta do modelo 1:", resposta1)
+class InputData(BaseModel):
+    llm_model:str
+    text: str
 
-resposta2 = chatbot_deepseek.chat("Quais os cinco principais sintomas do Alzheimer?")
-print("Resposta do modelo 2:", resposta2)
+@app.post("/predict")
+def predict(data: InputData):
+    chatbot_qwen = Model()
+    chatbot_deepseek = Model('deepseek-r1-distill-llama-70b')
+    
+    if data.llm_model == '':
+        return {"message": "Modelo não informado", "input": data.text}
+    
+    if data.text == '':
+        return {"message": "texto não informado", "input": data.text}
+    
+
+    if data.llm_model == 'model1':
+        response = chatbot_qwen.chat(data.text)
+        return {"model": "model1", "response": response}
+    elif data.llm_model == 'model2':
+        response = chatbot_deepseek.chat(data.text)
+        return {"model": "model2", "response": response}
+    else:
+        return {"message": "Modelo não reconhecido", "input": data.text}
+    
+
+# Para rodar o servidor: uvicorn main:app --reload

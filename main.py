9c68eff2c93ef27306931736a6f3e07c14e3995e
ogustavoalves <gustavodossantos1704@gmail.com>
2025-03-15 17:28:36 -0300

    :sparkles: feat: stream response

--- main.py
+++ main.py
@@ -1,46 +1,8 @@
-# from connection import Models
-
-# chatbot = Models()
-
-# resposta1 = chatbot.chat_1("O que é Alzheimer?")
-# print("Resposta do modelo 1:", resposta1)
-
-
-# resposta2 = chatbot.chat_1("Quais são os sintomas do Alzheimer?")
-# print("Resposta do modelo 2:", resposta2)
-
-
-
 from llm_chatbot import Model
-from fastapi import FastAPI
-from pydantic import BaseModel
 
-app = FastAPI()
+chat1 = Model()
+chat2 = Model('deepseek-r1-distill-llama-70b')
 
-class InputData(BaseModel):
-    llm_model:str
-    text: str
-
-@app.post("/predict")
-def predict(data: InputData):
-    chatbot_qwen = Model()
-    chatbot_deepseek = Model('deepseek-r1-distill-llama-70b')
-    
-    if data.llm_model == '':
-        return {"message": "Modelo não informado", "input": data.text}
-    
-    if data.text == '':
-        return {"message": "texto não informado", "input": data.text}
-    
-
-    if data.llm_model == 'model1':
-        response = chatbot_qwen.chat(data.text)
-        return {"model": "model1", "response": response}
-    elif data.llm_model == 'model2':
-        response = chatbot_deepseek.chat(data.text)
-        return {"model": "model2", "response": response}
-    else:
-        return {"message": "Modelo não reconhecido", "input": data.text}
-    
-
-# Para rodar o servidor: uvicorn main:app --reload
+response = chat2.chat('Principais sintomas do Alzheimer')
+for chunk in response:
+    print(chunk, end="", flush=True)
